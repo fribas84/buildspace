30345a0ee4170c89ca9adb82e3356a1bd9285d16
“Fernando <fribas84@gmail.com>
2022-01-23 11:54:10 -0300

    storing data into the SC

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,38 @@
 const main = async () => {
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
-  };
+  
+  // get the owner of the contract address[0] and another addr
+  const [owner, randomPerson1, randomPerson2] = await hre.ethers.getSigners();
+  
+  //Get the deployed instance
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  
+  // Validate Addresses 
+  console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+  waveCount = await waveContract.getTotalWaves();
+
+
+  //calling from another address 
+  waveTxn = await waveContract.connect(randomPerson1).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  //calling from another address 
+  waveTxn = await waveContract.connect(randomPerson2).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+
+};
   
   const runMain = async () => {
     try {
